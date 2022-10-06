76081318b9e6a1443335dc092f7e1307b62a80bf
Cha-er-si <vpcharles31@gmail.com>
2022-10-06 16:01:02 +0800

    Tutorial Done

--- main.js
+++ main.js
@@ -228,3 +228,113 @@ Arrays
 
 // const addNums = (num1 = 1, num2 = 1) => num1 + num2;
 // console.log(addNums());
+
+/* Constructer Function */
+// Function
+// function Person(firstName, lastName, dob){
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     // this.dob = dob;
+//     this.dob = new Date(dob);
+// }
+
+// Person.prototype.getBirthYear = function() {
+//     return this.dob.getFullYear();
+// }
+// Person.prototype.getFullName = function() {
+//     return `${this.firstName} ${this.lastName}`;
+// }
+
+// Class
+// class Person {
+//     constructor(firstName, lastName, dob) {
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.dob = new Date(dob);
+//     }
+
+//     getBirthYear() {
+//         return this.dob.getFullYear();
+//     }
+
+//     getFullName() {
+//         return `${this.firstName} ${this.lastName}`;
+//     }
+// }
+
+// Instantiate an object
+// const person1 = new Person('John', 'Doe', '4-3-1980');
+// const person2 = new Person('Mary', 'Smith', '3-5-1990');
+// console.log(person1);
+// console.log(person2);
+// console.log(person2.dob.getFullYear());
+// console.log(person1.getBirthYear());
+// console.log(person1.getFullName());
+
+/* DOM */
+
+// Selectors
+// - Single Element
+// const form = document.getElementById('my-form');
+// console.log(document.querySelector('.container'));
+// console.log(form);
+
+// - Multiple Element
+// console.log(document.querySelectorAll('.item'));
+// console.log(document.getElementsByClassName('item'));
+// console.log(document.getElementsByTagName('li'));
+// const items = document.querySelectorAll('.item');
+// items.forEach((item) => console.log(item));
+
+// const ul = document.querySelector('.items');
+
+// ul.remove();
+// ul.lastElementChild.remove();
+// ul.firstElementChild.textContent = 'Hello';
+// ul.children[1].innerText = 'Brad';
+// ul.lastElementChild.innerHTML = '<h1>HELLO</h1>';
+
+// const btn = document.querySelector('.btn');
+// btn.style.background = 'red';
+
+// const btn = document.querySelector('.btn');
+// btn.addEventListener('mouseout', (e) => {
+//     e.preventDefault();
+//     // console.log(e.target.className);
+//     document.querySelector('#my-form').style.background = '#ccc';
+//     document.querySelector('body').classList.add('bg-dark');
+//     document.querySelector('.items').lastElementChild.innerHTML = '<h1>HELLO</h1>';
+// });
+
+/* Form */
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e){
+    e.preventDefault();
+
+    if(nameInput.value === '' || emailInput.value === ''){
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all fields';
+
+        setTimeout(() => msg.remove(), 3000);
+    }
+    else{
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+
+        userList.appendChild(li);
+
+        // Clear the fields
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+
+    console.log(nameInput.value);
+}
