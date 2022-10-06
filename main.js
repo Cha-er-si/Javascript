37d5413b96a270c2f510882b00c5a727915188a0
Cha-er-si <vpcharles31@gmail.com>
2022-10-06 09:54:32 +0800

    First Commit

---
+++ main.js
@@ -0,0 +1,43 @@
+// alert('Hello World!');
+// console.log('Hello World!');
+// console.error("An Error");
+// console.warn('A warning');
+
+// Variables and How to set them
+// var (not recommended), let, const
+// let can reassign values
+
+// let age = 30;
+// // const age = 30;
+// age = 31;
+
+// console.log(age);
+
+// Data Types
+// String, Numbers, Boolean, null, undefined, Symbol
+// const name = "John";
+// const age = 30;
+// const rating = 4.5;
+// const isCool = true;
+// const x = null;
+// const y = undefined;
+// let z;
+
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof rating);
+// console.log(typeof isCool);
+// console.log(typeof x);
+// console.log(typeof y);
+// console.log(typeof z);
+
+// Concatenation
+// const name = "John";
+// const age = 30;
+
+// console.log('My name is ' + name + ' and I am ' + age);
+// Template String
+// console.log(`My name is ${name} and I am ${age}`);
+// const hello = `My name is ${name} and I am ${age}`;
+// console.log(hello);
+
